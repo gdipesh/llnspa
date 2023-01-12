5d0ee245a0af3a65efcc841970dcb86aa2578b75
Dipesh Gautam <83548128+gdipesh@users.noreply.github.com>
2023-01-12 23:46:32 +0545

    commit final

--- assets/js/model.js
+++ assets/js/model.js
@@ -1,1 +1,9 @@
-var closeBtn=document.getElementsByClassName("btn-close")[0],killModal=document.querySelector("#killModal"),modal=document.getElementById("myModal");closeBtn.addEventListener("click",function(){modal.classList.replace("d-block","d-none")}),killModal.addEventListener("click",function(){modal.classList.replace("d-block","d-none")});
+var closeBtn = document.getElementsByClassName("close")[0],
+killModal = document.querySelector("#booknow"),
+modal = document.getElementById("myModal");
+closeBtn.addEventListener("click", function () {
+  modal.classList.replace("d-block", "d-none");
+}),
+  killModal.addEventListener("click", function () {
+    modal.classList.replace("d-block", "d-none");
+  });
