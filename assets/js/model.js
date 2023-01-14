186ab13f21e5b4ee9294b4404c1ba818f1b47d15
Dipesh Gautam <83548128+gdipesh@users.noreply.github.com>
2023-01-15 00:00:41 +0545

    error fixed

--- assets/js/model.js
+++ assets/js/model.js
@@ -1,9 +1,9 @@
-var closeBtn = document.getElementsByClassName("close")[0],
-killModal = document.querySelector("#booknow"),
-modal = document.getElementById("myModal");
+var closeBtn = document.getElementsByClassName("close")[0]
+var killModal = document.querySelector("#booknow") ;
+var modal = document.getElementById("myModal");
 closeBtn.addEventListener("click", function () {
   modal.classList.replace("d-block", "d-none");
-}),
+});
   killModal.addEventListener("click", function () {
     modal.classList.replace("d-block", "d-none");
   });
